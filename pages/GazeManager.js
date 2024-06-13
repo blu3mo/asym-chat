952c45e7be0d8bf789118cd2cba3fe80e07cc711
Bluemo <shutaro.aoyama@gmail.com>
2024-06-13 17:58:44 +0900

    fix shortcut

--- pages/GazeManager.js
+++ pages/GazeManager.js
@@ -79,7 +79,7 @@ export default function GazeManager({ roomId, userName }) {
       console.log("roomId: ", roomId);
         // handler object
         const handleEvent = (ev) => {
-          if (ev.code === 'KeyG' && ev.ctrlKey) {
+          if (ev.code === 'KeyG' && ev.ctrlKey || ev.code === 'KeyG' && ev.code === 'KeyH') {
             if (isWebGazerLoaded) {
               console.log('WebGazer is already loaded.');
               return;
